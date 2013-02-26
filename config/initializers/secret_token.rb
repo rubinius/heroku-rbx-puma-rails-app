edcc6182f9d99105d7ceb2e56eb92fbe0ae75aae
Shane Becker <veganstraightedge@gmail.com>
2013-02-26 01:30:17 -0800

    vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuRbxPumaRailsApp::Application.config.secret_token = '5c17b36724152e6365f8e5027112573c36c5aecd3f0268f51636d0c6e81ee3a4216293d2df33148d0b2ad1e37b08fad4d0ad9b0bdc87d102d774b481e709ab9d'
